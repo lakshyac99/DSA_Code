e1f1f2ce3926ea059fb8ee9190dfcbca24888971
Lakshya Singh <lakshyac99@gmail.com>
2024-08-31 00:24:05 +0530

    Taking input from user.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        Scanner S = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int a = S.nextInt();
+        System.out.println("The entered number is: " + a);
     }
 }

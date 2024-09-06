de14bcfcea9aa25fdaa46d31ddf4467bc516f278
Lakshya Singh <lakshyac99@gmail.com>
2024-09-06 11:02:31 +0530

    learning recursion

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner S = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int a = S.nextInt();
-        System.out.println("The entered number is: " + a);
+        int n = 5;
+        for(int i = 0; i < 5; i++){
+            message();
+        }
+    }
+
+    static void message(){
+        System.out.println("Hello World!!");
     }
 }
